ae4d547ab3d176b1ea6262de923fd987bfb5ef47
Stefan Simon <stefan.j.c.simon@gmail.com>
2026-05-16 23:34:02 +0200

    [*] Insert data into page

--- src/page_as_pointer/mod.rs
+++ src/page_as_pointer/mod.rs
@@ -1,16 +1,22 @@
 use core::slice;
 
+use thiserror::Error;
+
 const PAGE_SIZE: usize = 1024;
-const SLOT_SIZE: usize = 8;
+const SLOT_SIZE: usize = 12;
 const PAGE_HEADER_SIZE: usize = 16;
+
+#[derive(Debug, Error)]
+#[error("An error occurred")]
+struct SomeError;
 
 #[repr(C)]
 #[derive(Clone, Copy)]
 struct Slot {
+    id: i32,
     offset: i32,
     length: i32,
 }
-
 
 #[repr(C)]
 #[derive(Clone, Copy)]
@@ -45,14 +51,47 @@ impl Page {
         }
     }
 
-    pub fn allocate_slot(&mut self) -> &mut Slot {
+    fn allocate_slot(&mut self) -> &mut Slot {
         let header = self.header_mut();
         let next = header.slots_offset as usize + PAGE_HEADER_SIZE;
+        let next_id = header.slots_offset as usize / SLOT_SIZE;
         header.slots_offset += SLOT_SIZE as i32;
         let p = self.bytes[next..next + SLOT_SIZE].as_mut_ptr() as *mut Slot;
         unsafe {
-            &mut *p
+            let slot = &mut *p;
+            slot.id = next_id as i32;
+            slot
         }
+    }
+
+    fn size(&self) -> i32 {
+        let header = self.header();
+        let tuple_size = PAGE_SIZE as i32 - header.data_offset;
+        PAGE_SIZE as i32 - PAGE_HEADER_SIZE as i32 - header.slots_offset - tuple_size
+    }
+
+    pub fn read_slot(&self, slot: &Slot) -> &[u8] {
+        let from = slot.offset as usize;
+        let to = (slot.offset + slot.length) as usize;
+        &self.bytes[from..to]
+    }
+
+    pub fn insert_record(&mut self, bytes: Vec<u8>) -> Result<(), SomeError> {
+        if bytes.len() + SLOT_SIZE > self.size() as usize {
+            return Err(SomeError);
+        }
+
+        let header = self.header_mut();
+        let end_of_data = header.data_offset as usize;
+        header.data_offset -= bytes.len() as i32;
+
+        let slot = self.allocate_slot();
+        slot.length = bytes.len() as i32;
+        slot.offset = (end_of_data - bytes.len()) as i32;
+        
+        self.bytes[end_of_data - bytes.len()..end_of_data].copy_from_slice(&bytes);
+
+        Ok(())
     }
 
     pub fn slots(&self) -> &[Slot] {
@@ -85,6 +124,22 @@ mod tests {
     }
 
     #[test]
+    fn should_write_and_read_slots() {
+        let mut page = Page::new(1);
+        page.insert_record(vec![1, 2, 3]).unwrap();
+        page.insert_record(vec![4, 5, 6]).unwrap();
+
+        let slots = page.slots();
+
+        assert_eq!(slots.len(), 2);
+        let bytes_slot_0 = page.read_slot(&slots[0]);
+        let bytes_slot_1 = page.read_slot(&slots[1]);
+
+        assert_eq!(bytes_slot_0, &[1, 2, 3]);
+        assert_eq!(bytes_slot_1, &[4, 5, 6]);
+    }
+
+    #[test]
     fn should_create_slots_and_can_read_slice() {
         let mut page = Page::new(1);
 
@@ -103,10 +158,12 @@ mod tests {
         assert_eq!(slots.len(), 2);
 
         let slot1 = slots[0];
+        assert_eq!(slot1.id, 0);
         assert_eq!(slot1.offset, 10);
         assert_eq!(slot1.length, 11);
 
         let slot2 = slots[1];
+        assert_eq!(slot2.id, 1);
         assert_eq!(slot2.offset, 20);
         assert_eq!(slot2.length, 21);
     }
